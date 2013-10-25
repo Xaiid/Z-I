1c2b869b757dff50e056962e6f424c773c14266a
Narciso Guillen <narciso.guillen@tangosource.com>
2013-10-25 10:40:20 -0500

    Start create user endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -4,4 +4,7 @@ module.exports = function(app){
   app.get('/', pagesController.index.bind(pagesController));
   app.get('/login', pagesController.login.bind(pagesController));
   app.get('/game', pagesController.game.bind(pagesController));
+
+  //Game actions
+  app.post('/user/create', user_controller.create.bind(user_controller));
 };
