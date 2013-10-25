1cafd164c2ecda9a3b2db9d1bb62c33a7abb7141
Jeaneth <xaid27@gmail.com>
2013-10-25 12:45:32 -0500

    creating users and rooms

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,50 @@
+var mongoose   = require('mongoose');
+var userSchema = require('../models/user');
+var roomSchema = require('../models/room');
+
+var Room     = mongoose.model('room', roomSchema);
+var User     = mongoose.model('user', userSchema);
+
+var onError = function(err){
+  log(err);
+};
+
 module.exports = {
 
   create: function(req, res){
-    res.send('Working on it');
-  }
+    var user = req.body;
 
+    _.extend(user, {
+      alive: true,
+      waiting: false,
+    });
+
+    var newUser = new User(user);
+    newUser.save(onError);
+
+    //Search for room available
+
+    Room.findOne({$where: 'this.players.length < 4'}).exec(function(err, room){
+
+      if(!room){
+        var newRoom = new Room({
+          players: [newUser.id],
+          zombies:[],
+          level: 1
+        });
+        newRoom.save(onError);
+        newUser.update({roomId: newRoom.id}, onError);
+        log(user, newUser);
+      }
+
+      if(room){
+        room.players.push(newUser.id);
+        room.save(onError);
+        newUser.update({roomId: room.id}, onError);
+        log(user, newUser);
+      }
+
+      res.send("yupiii");
+    });
+  }
 };
